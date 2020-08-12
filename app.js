13bc08f80927f6edad7be42690a3b36651a451be
Santu Nandi <santunandi.nandi@gmail.com>
2020-08-12 18:19:25 +0530

    Push the changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors=require('cors');
+var cors = require('cors');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
 var fileUpload = require('express-fileupload');
@@ -25,8 +25,8 @@ app.engine('ejs', require('ejs').renderFile);
 app.use(cors());
 app.options('*', cors());
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,19 +35,19 @@ app.use(fileUpload());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   //console.log("corsssssssssss111111")
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.all("/*", function(req, res, next){
- //   console.log("corsssssssssss",res)
+app.all("/*", function (req, res, next) {
+  //   console.log("corsssssssssss",res)
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  
+
   next();
 });
 
@@ -56,21 +56,28 @@ app.all("/*", function(req, res, next){
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
- // console.log("corsssssssssss222222222")
-  app.use(function(err, req, res, next) {
-   // console.log("corsssssssssss33333333")
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   console.log("corsssssssssss222222222")
+//   app.use(function (err, req, res, next) {
+//     // console.log("corsssssssssss33333333")
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
+  if (typeof err == "string") {
+    console.log(typeof err);
+    return res.send({
+      success: false,
+      message: err
+    });
+  }
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- models/matchController.js
+++ models/matchController.js
@@ -1,19 +1,21 @@
+const util = require('util');
 var db = require('../dbconnection');
+const query = util.promisify(db.query).bind(db);
 
 module.exports = {
 
  placeBet: async (req, res, next) => {
-  console.log("called");
   try {
-   let sql = `INSERT INTO single_bet_info 
-   (market_id,market_status, market_type, market_start_time, market_end_time, description, event_name, bet_time, user_id, bet_id, bet_status,exposure,runner_name,stake,odd,placed_odd,last_odd,p_and_l,amount, available_balance, protential_profit,user_ip,settled_time)
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
-   let result = await db.query(sql, [req.body.market_id, req.body.market_status, req.body.market_type, req.body.market_start_time, req.body.market_end_time, req.body.description, req.body.event_name, req.body.bet_time, req.body.user_id, req.body.bet_id, req.body.bet_status, req.body.exposure, req.body.runner_name, req.body.stake, req.body.odd, req.body.placed_odd, req.body.last_odd, req.body.p_and_l, req.body.amount, req.body.available_balance, req.body.protential_profit, req.body.user_ip, req.body.settled_time]);
-   console.log(result);
+   const userId = "2750231N007";
+   const userData = await query("SELECT * from punter where punter_id =?", [userId]);
+   if (!(userData && userData.length)) return next("User data not found");
+   const reqData = { ...req.body, user_id: userId, bet_status: 1 };
+   let result = await query('INSERT INTO single_bet_info SET ?', reqData);
+   console.log(result.insertId);
    return res.send({
     success: true,
     message: "Bet placed success",
-    // data: result
+    data: result
    })
   } catch (err) {
    console.log(err)
@@ -23,7 +25,6 @@ module.exports = {
     err: err
    })
   }
-
  }
 
 };
